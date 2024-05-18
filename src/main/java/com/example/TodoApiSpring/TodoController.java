92dfa4a2c08bd89bbee851aa828d0d90e5f80658
Abhijit Mishra <abhijitmishraak10@gmail.com>
2024-05-18 16:10:14 +0530

    Added todo creation and statusCode

--- src/main/java/com/example/TodoApiSpring/TodoController.java
+++ src/main/java/com/example/TodoApiSpring/TodoController.java
@@ -1,26 +1,33 @@
 package com.example.TodoApiSpring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class TodoController {
-    private static List<Todo>todos;
+    private static List<Todo>todoList;
 
     public TodoController(){
 
-        todos = new ArrayList<>();
-        todos.add(new Todo(1,true,"Todo-1",1));
-        todos.add(new Todo(2,false,"Todo-2",2));
+        todoList = new ArrayList<>();
+        todoList.add(new Todo(1,true,"Todo-1",1));
+        todoList.add(new Todo(2,false,"Todo-2",2));
     }
 
     @GetMapping("/todos")
      public List<Todo>getTodos(){
-        return todos;
+        return todoList;
     }
+    @PostMapping("/todos")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Todo createTodo(@RequestBody Todo newtodo){
+        todoList.add(newtodo);
+        return newtodo;
+    }
+
 
 
 
